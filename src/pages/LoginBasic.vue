18ee5adf5a0d0268614c3b52304dcd2f0b18e5df
Nibitor21 <141552559+Nibitor21@users.noreply.github.com>
2024-12-15 21:09:08 +0100

    Update LoginBasic.vue

--- src/pages/LoginBasic.vue
+++ src/pages/LoginBasic.vue
@@ -75,7 +75,7 @@ export default defineComponent({
         'password': this.password
       }
 
-      axios.post('https://timecatcher.lnbits.de/api/v1/auth', data)
+      axios.post('https://lnbits1.apfelblitz.de/api/v1/auth', data)
         .then(response => {
           this.$store.setLoginSession(response.data);
           this.$router.push('/')
